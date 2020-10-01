d91eaf50b8277838ede143985073ef6eb109ac07
rafaelbreno <rafael_breno@outlook.com>
2020-10-01 19:35:36 -0300

    Add method to pattern json responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param array $data
+     * @param bool $success
+     * @param array $addons
+     * @param int $status
+     * @return JsonResponse
+     */
+    protected function returnJson(
+        array $data,
+        bool $success,
+        array $addons = array(),
+        int $status = 200) : JsonResponse
+    {
+        $data = array_merge_recursive($data, $addons);
+
+        return response()->json([
+            'success' => $success,
+            'data' => $data
+        ], $status);
+    }
 }
