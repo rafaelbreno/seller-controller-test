a924b33683344fa9b4a964c21f3dc3634e93d130
rafaelbreno <rafael_breno@outlook.com>
2020-10-02 06:50:08 -0300

    Add @include tag to include user's register form

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,8 +1,18 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="row justify-content-center">
-        <div class="col-10">
+    <div class="card w-75 mx-auto mt-3">
+        <div class="card-header">
+            <div class="row justify-content-around">
+                <div class="col-6 text-left">
+                    Seller's List
+                </div>
+                <div class="col-6 text-right">
+                    @include('partials.modal-create-user')
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
             <table class="table" id="usersTable">
                 <thead>
                 <tr>
@@ -17,30 +27,31 @@
 @endsection
 
 @push('scripts')
-<script>
-    function getUsers() {
-        return axios({
-            url: "{{ url('/get-user') }}",
-            method: "GET"
-        }).then(resp => {
-            return resp.data;
-        }).catch(err => {
-            return err.response;
+    <script>
+        function getUsers() {
+            return axios({
+                url: "{{ url('/get-user') }}",
+                method: "GET"
+            }).then(resp => {
+                return resp.data;
+            }).catch(err => {
+                return err.response;
+            });
+        }
+
+        $(document).ready(function () {
+            getUsers()
+                .then(data => {
+                    console.log(data);
+                    $('table[id="usersTable"]').DataTable({
+                        data: data,
+                        columns: [
+                            {data: 'name'},
+                            {data: 'email'},
+                            {data: 'created_at'},
+                        ]
+                    });
+                })
         });
-    }
-    $(document).ready(function () {
-        getUsers()
-            .then(data => {
-                console.log(data);
-                $('table[id="usersTable"]').DataTable({
-                    data: data,
-                    columns: [
-                        {data: 'name'},
-                        {data: 'email'},
-                        {data: 'created_at'},
-                    ]
-                });
-            })
-    });
-</script>
+    </script>
 @endpush
