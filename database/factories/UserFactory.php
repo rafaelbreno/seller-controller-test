eda11170ae8fe805638a4fd120e8f748716d6eb4
rafaelbreno <rafael_breno@outlook.com>
2020-10-02 06:49:14 -0300

    Remove nonexistent values from UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => Hash::make(Str::random(8)), // password
-            'remember_token' => Str::random(10),
+            'email' => $this->faker->unique()->safeEmail
         ];
     }
 }
