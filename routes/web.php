bc7730e519180bed85ea4f70a4076ed6fe86c2a6
rafaelbreno <rafael_breno@outlook.com>
2020-10-02 08:21:52 -0300

    Update route names

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/seller');
+
+Route::resource('/seller', \App\Http\Controllers\UserController::class);
