04facd736faf0f47cf45d08cfac6b44690a71ace
rafaelbreno <rafael_breno@outlook.com>
2020-10-03 17:25:25 -0300

    Create Sale's factory

---
+++ database/factories/SaleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Sale;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SaleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Sale::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'sale_value' => $this->faker->randomFloat(2, 1.00, 10000)
+        ];
+    }
+}
