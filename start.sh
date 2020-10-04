67e4e483a16f11f4bb2bb7cf085e72dcc9622b8e
rafaelbreno <rafael_breno@outlook.com>
2020-10-04 19:22:24 -0300

    Add comments and artisan test command

--- start.sh
+++ start.sh
@@ -1,19 +1,32 @@
+# Setting up storage access level
 chmod -R 777 storage/ storage/*
 
+# Copying env file
 cp .env.example .env
 
+# Running docker at background
 docker-compose up -d
 
+# Generating env key
 docker-compose exec app php artisan key:generate
 
+# Running tests
+docker-compose exec app php artisan test
+
+# Fresh migrating
 docker-compose exec app php artisan migrate:fresh
 
+# Seeding database
 docker-compose exec app php artisan db:seed
 
+# Cleaning view cache
 docker-compose exec app php artisan view:clear
 
+# Cleaning route cache
 docker-compose exec app php artisan route:clear
 
+# Cleaning app cache
 docker-compose exec app php artisan cache:clear
 
+# Running cron job (1 min)
 docker-compose exec app .docker/bin/cron.sh
