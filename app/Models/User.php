3a10eaeb72ed01dbb1fdbcea6ae552be06f07992
rafaelbreno <rafael_breno@outlook.com>
2020-10-04 19:50:29 -0300

    Remove User's model default config/attrs

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,18 +20,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
+        'email'
     ];
 
     /**
