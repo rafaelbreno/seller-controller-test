cefffe3da6cb43f4a2030ff3dc26c473be0b5d0e
rafaelbreno <rafael_breno@outlook.com>
2020-10-04 19:19:02 -0300

    Config TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use Faker\Factory;
+use Faker\Generator;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected Generator $faker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Factory::create();
+    }
+
+    public function tearDown(): void
+    {
+        $this->artisan('migrate:reset');
+        parent::tearDown();
+    }
 }
