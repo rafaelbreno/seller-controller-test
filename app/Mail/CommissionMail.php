9ddf0f699254260931e3b369d154c3758e185e36
rafaelbreno <rafael_breno@outlook.com>
2020-10-04 10:52:26 -0300

    Create simple Mail class to build email body

---
+++ app/Mail/CommissionMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CommissionMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var array
+     */
+    private array $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mail.index', $this->data);
+    }
+}
